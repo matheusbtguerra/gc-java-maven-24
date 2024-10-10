794d36763590bafd3391eaff8a78042291fbafc5
alunojava-03 <alunojava-03@eldorados-MacBook-Pro.local>
2024-10-10 14:35:22 -0300

    upando maven e Junit ao projeto

---
+++ src/main/java/aula08/exercicio25/Exe25.java
@@ -0,0 +1,7 @@
+package aula08.exercicio25;
+
+public class Exe25 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/aula08/exercicio25/Operacoes.java
@@ -0,0 +1,5 @@
+package aula08.exercicio25;
+
+public class Operacoes {
+    
+}
