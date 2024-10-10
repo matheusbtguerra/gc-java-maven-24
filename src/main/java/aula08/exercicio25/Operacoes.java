d821b864f96636fac647d55307f9f70204c6190e
alunojava-03 <alunojava-03@eldorados-MacBook-Pro.local>
2024-10-10 15:26:20 -0300

    upando exercício 25 aula 08

--- src/main/java/aula08/exercicio25/Operacoes.java
+++ src/main/java/aula08/exercicio25/Operacoes.java
@@ -1,5 +1,23 @@
 package aula08.exercicio25;
 
 public class Operacoes {
-    
+    public Double somaDeDoisNumeros(Double num1, Double num2) {
+        return num1 + num2;
+    }
+
+    public Double subtracaoDeDoisNumeros(Double num1, Double num2) {
+        return num1 - num2;
+    }
+
+    public Double multiplicacaoDeDoisNumeros(Double num1, Double num2) {
+        return num1 * num2;
+    }
+
+    public Double divisaoDeDoisNumeros(Double num1, Double num2) {
+        if (num2 == 0) {
+            return null;
+        } else {
+            return num1 / num2;
+        }
+    }
 }
---
+++ src/test/java/com/java/caldeira/aula09/exercicio25/OperacoesTests.java
@@ -0,0 +1,40 @@
+package com.java.caldeira.aula09.exercicio25;
+
+import aula08.exercicio25.Operacoes;
+import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OperacoesTests {
+    Operacoes operacoes = new Operacoes();
+
+    @Test
+    void verificaSomaDeDoisNumeros(){
+        Double resutado = operacoes.somaDeDoisNumeros(5.0, 2.0);
+        Assertions.assertEquals(7.0, resutado);
+    }
+
+    @Test
+    void verificaSubtracaoDeDoisNumeros(){
+        Double resutado = operacoes.subtracaoDeDoisNumeros(5.0, 2.0);
+        Assertions.assertEquals(3.0, resutado);
+    }
+
+    @Test
+    void verificaMultiplicacaoDeDoisNumeros(){
+        Double resutado = operacoes.multiplicacaoDeDoisNumeros(5.0, 2.0);
+        Assertions.assertEquals(10.0, resutado);
+    }
+
+    @Test
+    void verificaDivisaoDeDoisNumeros(){
+        Double resutado = operacoes.divisaoDeDoisNumeros(5.0, 2.0);
+        Assertions.assertEquals(2.5, resutado);
+    }
+
+    @Test
+    void verificaDivisaoDeDoisNumerosComSegundoNumeroSendoZero(){
+        Double resutado = operacoes.divisaoDeDoisNumeros(5.0, 0.0);
+        Assertions.assertEquals(null, resutado);
+    }
+}
