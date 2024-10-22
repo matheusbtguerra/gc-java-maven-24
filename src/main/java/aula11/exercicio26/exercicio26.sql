23597484def80ad2b0d05591590c5efe3bb96fc4
matheusbtguerra <matheus05guerra@gmail.com>
2024-10-22 11:03:58 -0300

    Upando exercício 26

---
+++ src/main/java/aula11/exercicio26/exercicio26.sql
@@ -0,0 +1,22 @@
+CREATE TABLE autor (
+    autorID SERIAL PRIMARY KEY,
+    nome VARCHAR(255),
+    dataNascimento DATE
+);
+
+CREATE TABLE livro (
+    livroID SERIAL PRIMARY KEY,
+    titulo VARCHAR(255),
+    anoPublicacao INT,
+    autorID INT REFERENCES autor(autorID)
+);
+
+CREATE TABLE emprestimo (
+    emprestimoID SERIAL PRIMARY KEY,
+    livroID INT REFERENCES livro(livroID),
+	nomePessoa VARCHAR(255),
+	dataEmprestimo DATE,
+	dataDevolucao DATE 
+);
+
+
