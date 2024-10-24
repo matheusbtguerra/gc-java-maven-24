ee9ca788d429a932862616b6f88afc7681e5fd24
alunojava-03 <alunojava-03@eldorados-MacBook-Pro.local>
2024-10-24 16:48:21 -0300

    atualizando o exercício

--- src/main/java/aula11/exercicio27/exercicio27.sql
+++ src/main/java/aula11/exercicio27/exercicio27.sql
@@ -16,9 +16,21 @@ CREATE TABLE emprestimo (
     livroID INT REFERENCES livro(livroID),
 	nomePessoa VARCHAR(255),
 	dataEmprestimo DATE,
-	dataDevolucao DATE 
+	dataDevolucao DATE
 );
 
-INSERT INTO autor (autorID, nome, dataNascimento) VALUES (1,"Matheus Guerra", 10-14-2005);
+INSERT INTO autor (autorID, nome, dataNascimento) VALUES (1,'Matheus Guerra', '10-14-2005');
+INSERT INTO autor (autorID, nome, dataNascimento) VALUES (2,'Rafael Mainiere', '05-08-2000');
+INSERT INTO autor (autorID, nome, dataNascimento) VALUES (3,'Emerson Garcia', '06-12-2002');
 
+INSERT INTO livro (livroID, titulo, anoPublicacao, autorID) VALUES (DEFAULT,'A espera de um vinagre', 2010, 1);
+INSERT INTO livro (livroID, titulo, anoPublicacao, autorID) VALUES (DEFAULT,'Um bauduco no pedaço', 2015, 2);
+INSERT INTO livro (livroID, titulo, anoPublicacao, autorID) VALUES (DEFAULT,'O exterminador do X-tudo', 2009, 3);
 
+INSERT INTO emprestimo VALUES (DEFAULT, 2, 'Henrique Alíbio','06-12-2024','12-12-2024');
+INSERT INTO emprestimo VALUES (DEFAULT, 3, 'Henrique Alíbio','12-08-2024','12-09-2024');
+INSERT INTO emprestimo VALUES (DEFAULT, 4, 'Henrique Alíbio','09-04-2024','08-04-2024');
+
+SELECT * FROM autor;
+SELECT * FROM livro;
+SELECT * FROM emprestimo;
